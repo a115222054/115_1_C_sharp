37d3283ff5a740604ec62272a7c2e30e2daa9f38
a115222054 <a115222054@mail.shu.edu.tw>
2026-09-24 17:21:18 +0800

    建立

---
+++ 115_09_24/Tutorial_2_2/Tutorial_2_2/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tutorial_2_2.Startup))]
+namespace Tutorial_2_2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
